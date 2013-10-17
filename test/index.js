ed31d3c2d7cea990f9af36499d29a04dbd68a288
daewon <blueiur@gmail.com>
2013-10-17 19:33:23 +0900

    rename hi -> is

--- test/index.js
+++ test/index.js
@@ -1,4 +1,4 @@
-var hi = require('../istool');
+var is = require('../istool');
 var assert = require('assert');
 
 // test object
@@ -7,9 +7,9 @@ var Person = function(firstName, lastName, yearOfBirth) {
   this.lastName = lastName;
 
   this.fullName = function() {
-    return firstName + " " + lastName; 
+    return firstName + " " + lastName;
   };
-  
+
   this.getAge = function() {
     return (new Date()).getFullYear() - yearOfBirth;
   };
@@ -26,40 +26,38 @@ var persons = [
 
 var nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-describe('hi.eq', function() {
+describe('is.eq', function() {
   it('should equal', function() {
-    assert.equal(hi.eq('daewon')('daewon'), true);
-    assert.equal(hi.eq('daewon')('Dennis'), false);
-    assert.deepEqual(nums.filter(hi.eq(2)), [2]);
-    assert.deepEqual(nums.filter(hi.eq(10)), [10]);
+    assert.equal(is.eq('daewon')('daewon'), true);
+    assert.equal(is.eq('daewon')('Dennis'), false);
+    assert.deepEqual(nums.filter(is.eq(2)), [2]);
+    assert.deepEqual(nums.filter(is.eq(10)), [10]);
   });
 });
 
-describe('hi.ne', function() {
+describe('is.ne', function() {
   it('should not equal', function() {
-    assert.equal(hi.ne('daewon')('Dennis'), true);
-    assert.equal(hi.ne('daewon')('daewon'), false);
-    assert.deepEqual(nums.filter(hi.ne(2)), [1, 3, 4, 5, 6, 7, 8, 9, 10]);
-    assert.deepEqual(nums.filter(hi.ne(10)), [1, 2, 3, 4, 5, 6, 7, 8, 9]);
+    assert.equal(is.ne('daewon')('Dennis'), true);
+    assert.equal(is.ne('daewon')('daewon'), false);
+    assert.deepEqual(nums.filter(is.ne(2)), [1, 3, 4, 5, 6, 7, 8, 9, 10]);
+    assert.deepEqual(nums.filter(is.ne(10)), [1, 2, 3, 4, 5, 6, 7, 8, 9]);
   });
 });
 
-describe('hi.peq', function() {
+describe('is.peq', function() {
   it('should equal', function() {
-    assert.deepEqual(persons.filter(hi.peq('lastName', 'jeong')), [persons[0]]);
+    assert.deepEqual(persons.filter(is.peq('lastName', 'jeong')), [persons[0]]);
   });
 });
 
-describe('hi.ieq', function() {
+describe('is.ieq', function() {
   it('should equal', function() {
-    assert.deepEqual(persons.filter(hi.ieq('getAge', 31)), [persons[0]]);
+    assert.deepEqual(persons.filter(is.ieq('getAge', 31)), [persons[0]]);
   });
 });
 
-describe('hi.ieq', function() {
+describe('is.ieq', function() {
   it('should equal', function() {
-    assert.deepEqual(persons.filter(hi.ieq('getAge', 31)), [persons[0]]);
+    assert.deepEqual(persons.filter(is.ieq('getAge', 31)), [persons[0]]);
   });
 });
-
-
